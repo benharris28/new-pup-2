49fc3c298d550413a69d4ac77eba8b2a663d7eb8
Ben Harris <benjaminharris28@gmail.com>
2020-06-23 10:03:03 -0400

    added all cards to checklist

--- src/Routes/Checklist/Checklist.js
+++ src/Routes/Checklist/Checklist.js
@@ -38,24 +38,83 @@ class Checklist extends React.Component {
                             <Meta className="checklist-card-text" title="Get the gear" description="Complete by June 25th" />
                         </Card>
                     </Link>
-                    <Card
-                        hoverable
-                        className="checklist-card"
-                        cover={<img className="checklist-card-image" alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-                    >
-                    <div className="card-content">
-                    <Meta 
-                        title="Europe Street beat" 
-                        description="www.instagram.com" 
-                    />
+                   
+                    
                 
-                    </div>
+                
+                    <Link to={'/gear'}>
+                        <Card
+                            className="checklist-card"
+                            hoverable
+                            cover={<img className="checklist-card-image" 
+                                        alt="pet food" 
+                                        src="https://i.insider.com/5b1fc0281ae6621a008b4fb2?width=1100&format=jpeg&auto=webp" />}
+                            
+                        >
+                            <Meta 
+                                className="checklist-card-text" 
+                                title="Find the best food" 
+                                description="Complete by June 25th" 
+                                />
+                        </Card>
+                    </Link>
+
+                    <Link to={'/gear'}>
+                        <Card
+                            className="checklist-card"
+                            hoverable
+                            cover={<img className="checklist-card-image" 
+                                        alt="find a vet" 
+                                        src="https://trupanion.com/blog/wp-content/uploads/2019/04/ThankYourVeterinarian-1024x682.jpg" />}
+                            
+                        >
+                            <Meta 
+                                className="checklist-card-text" 
+                                title="Find a local veterinarian" 
+                                description="Complete by June 27th" 
+                                />
+                        </Card>
+                    </Link>
+
+                    <Link to={'/gear'}>
+                        <Card
+                            className="checklist-card"
+                            hoverable
+                            cover={<img className="checklist-card-image" 
+                                        alt="puppy socialization" 
+                                        src="https://tailblazerspets.com/blog/wp-content/uploads/2015/02/golden-pups.png" />}
+                            
+                        >
+                            <Meta 
+                                className="checklist-card-text" 
+                                title="Sign up for puppy socialization" 
+                                description="Complete by July 1st" 
+                                />
+                        </Card>
+                    </Link>
+
+                    <Link to={'/gear'}>
+                        <Card
+                            className="checklist-card"
+                            hoverable
+                            cover={<img className="checklist-card-image" 
+                                        alt="24 hour guide" 
+                                        src="https://dogtime.com/assets/uploads/2011/03/puppy-development-1280x720.jpg" />}
+                            
+                        >
+                            <Meta 
+                                className="checklist-card-text" 
+                                title="Read Doggo's 24 hour survival guide" 
+                                description="Everything you needt to know, including how to get ready, give your first meal, housetrain, and more!" 
+                                />
+                        </Card>
+                    </Link>
 
                           
 
                 
                         
-                    </Card>
+                   
 
                     </Space>
                     </Content>
