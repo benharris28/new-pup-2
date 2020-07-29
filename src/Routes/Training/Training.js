2329e1269402d2fa033d163841e0f77d6ae66c87
Ben Harris <benjaminharris28@gmail.com>
2020-07-29 14:26:53 -0400

    updated pup socialization

--- src/Routes/Training/Training.js
+++ src/Routes/Training/Training.js
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom'
 import Trainers from '../../BackupData/Trainers'
 import ApiContext from '../../ApiContext';
 import MarkCompleteStandAlone from '../../Components/MarkCompleteStandAlone/MarkCompleteStandAlone'
-import { Layout, Content, Button, Alert, Card, Avatar } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import { Layout, Content, Button, Alert, Card, Avatar, Row, Col } from 'antd';
+import { UserOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
+
 
 class Training extends React.Component {
 
@@ -35,62 +36,89 @@ class Training extends React.Component {
                 title: 'Learn how to play appropriately with other dogs'
             },
 
-            
+
 
         ]
 
         return (
             <div>
                 <Layout>
-                    <Content
-                        //style={{ padding: '0 50px' }}
-                        className="page-content">
 
-                        <div className="back-to-checklist-button">
+                    <div className="guide-banner">
+
+                        <div className="guide-banner-image" />
+
+                        <div className="back-to-checklist-button-test">
                             <Link to='/checklist'>
                                 <Button
                                     type="primary">
-                                    Back to checklist
+                                    Back to Checklist
                                 </Button>
                             </Link>
 
                         </div>
 
-                        <div className="alert-box">
-                            <Alert
-                                message="Heads Up!"
-                                description="Experts recommend completing socialization before Russell is 12 weeks old (July 12th)"
-                                type="warning"
-                                showIcon
-                                closable
-                            />
+                        <div className="guide-banner-title">
+                            <h5>Russell's Checklist</h5>
+                            <h3>Puppy Socialization</h3>
+
                         </div>
 
-                        <div className="page-intro">
-                            <h1>Let's socialize your pup</h1>
-                        </div>
-                        <div>
-                            It's a scary world for new puppies - loud noises, lots of humans, vet visits, and other dogs! Puppy classes are extremely important to prepare your dog for this crazy world!
-                        </div>
-                        
-                        <div className="training-detail">
-                            <h4>What you'll learn</h4>
-                            <div className="training-learning-points">
-                                {points.map(point => 
-                                    <div className="training-action">
-                                        <Avatar className="training-avatar" size={50} icon={<UserOutlined />} />
-                                        <div className="training-content">
-                                        <h6>{point.title}</h6>
-                                        </div>
-                                        
-                                    </div>
-                            )}
-                            
+                    </div>
+                    <div className="content-section white">
+                    <div className="checklist-page content center">
+                        <div className="checklist-page-section-intro">
+                                <h3>Let's socialize your pup!</h3>
+                                <h5>It's a scary world for new puppies - loud noises, lots of humans, vet visits, and other dogs! Puppy classes are extremely important to prepare your dog for this crazy world!</h5>
+                            </div>
+                    </div>
+                    </div>
+
+                    <div className="content-section green">
+                        <div className="checklist-page content center">
+                            <div className="checklist-page-alert">
+                                <ExclamationCircleOutlined className="checklist-icon" />
+                                <h3>Heads up!</h3>
+                                <h5>Experts recommend completing socialization before Russell is 12 weeks old (July 12th)</h5>
                             </div>
 
                         </div>
+                    </div>
+
+                    <div className="content-section white">
+
+
+                        <div className="checklist-page content center">
+                            <div className="checklist-page-section-intro">
+                                <h3>Why should I sign up for puppy class?</h3>
+                                <h5>Puppy classes teach new pups vital skills to help them adapt and develop</h5>
+                            </div>
+                            <div className="nutrition-types-container">
+                                <Row
+                                    gutter={[40, 24]}
+                                >
+                                    {points.map(point =>
+
+                                    <Col
+                                        className="gutter-row" sm={24} md={12} lg={6}>
+                                        <div className="nutrition-type-item">
+                                            <div className="food-type-image-box">
+
+                                            </div>
+                                            <h4>{point.title}</h4>
+                                            
+                                        </div>
+                                    </Col>
+                                    )}
+                                </Row>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="content-section green">
+                        <div className="checklist-page content center">
                         <div>
-                            <h4>Here are some recommended puppy classes near you</h4>
+                            <h3>Here are some recommended puppy classes near you</h3>
                             {trainers.map(trainer =>
                                 <Card className="trainer-card" key={trainer.trainer_id} type="inner" title={trainer.name} extra={<a target='_blank' rel="noopener noreferrer" href={`${trainer.link}`}>Site</a>}>
                                     <p>{trainer.blurb}</p>
@@ -99,8 +127,20 @@ class Training extends React.Component {
                             )}
 
                         </div>
-                        <MarkCompleteStandAlone />
-                    </Content>
+
+                        </div>
+                    </div>
+                    <div className="next-section-button">
+                        <Link to='/guide'>
+                            <Button>
+                                Up Next: 24 Hour Guide
+                                    </Button>
+                        </Link>
+
+                    </div>
+
+
+                   
                 </Layout>
 
 
