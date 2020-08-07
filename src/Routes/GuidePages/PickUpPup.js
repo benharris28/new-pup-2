bf46b69cbbda7a9386f65b078403acb12522537e
Ben Harris <benjaminharris28@gmail.com>
2020-08-07 17:14:56 -0400

    updated

--- src/Routes/GuidePages/PickUpPup.js
+++ src/Routes/GuidePages/PickUpPup.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import Russ_car from '../../Assets/Russ_car.JPG'
-import { Layout, Content, Button } from 'antd';
+import { Layout, Content, Button, Row, Col } from 'antd';
+import stock_image from '../../Assets/stock_image.png'
 
 class PickUpPup extends React.Component {
     render() {
@@ -9,52 +10,102 @@ class PickUpPup extends React.Component {
         return (
             <div>
                 <Layout>
-                    <Content
-                        className="page-content">
-                        <div className="back-to-checklist-button">
-                            <Link to='/checklist'>
+                    <div className="survival-guide-banner">
+
+                        <div className="survival-guide-banner-image white" />
+
+                        <div className="back-to-checklist-button-test">
+                            <Link to='/guide'>
                                 <Button
                                     type="primary">
-                                    Back to Guide Menu
-                                    </Button>
+                                    Back to guide menu
+                                </Button>
                             </Link>
 
                         </div>
-                        <div className="intro">
-                         <img className="russ-image" src={Russ_car} alt="russell" />
-                        <h3>Time to pickup Russell!</h3>
-                        <div>
-                            Overview
-                                <ul>
-                                <li>Bring towels!</li>
-                                <li>Bring a friend</li>
-                                <li>Bring Russell's leash, harness, and tags</li>
 
-                            </ul>
-                        </div>
-                        </div>
-                        <div>
-                           <h4>The big day is here</h4> 
-                           <p>Get ready! Your life will change forever today in the best way possible! Get excited and don't forget a few handy tips</p>
+                        <div className="survival-guide-banner-title">
+                            <h5 className="survival-guide-title">Doggo Survival Guide</h5>
+                            <div className="step-counter"><h6>Step 2 of 5</h6></div>
+                            <h1>Pickup Pup</h1>
 
                         </div>
-                        <div>
-                            <h4>Bring towels!</h4>
-                            <p>Dogs can get nauseous and anxious during their first few trips in a car. Spread a few towels down to help catch / cleanup any mess</p>
 
-        
+                    </div>
+
+                    <div className="content-section dark-green">
+                    <div className="checklist-page content">
+                        <div className="checklist-page-alert font-black">
+
+                            <h3>The big day is here!</h3>
+                            <h5>Get ready! Your life will change forever today in the best way possible! Get excited and don't forget a few handy tips</h5>
                         </div>
-                        <div className="next-section-button">
-                                <Link to='/food'>
-                                    <Button>
-                                        Up Next: Pup is home!
-                                    </Button>               
-                                </Link>
-                                
 
+                    </div>
+                </div>
+
+                <div className="content-section white">
+                <div className="checklist-page content">
+                            <div className="checklist-page-section-intro">
+                                <h3>What you should bring</h3>
+                                <h5>Don't forget these items when going to pick up Russell!</h5>
                             </div>
+                            <div className="nutrition-types-container">
+                                <Row
+                                    gutter={[40, 24]}
+                                >
 
-                    </Content>
+                                    <Col
+                                        className="gutter-row" xs={24} sm={24} md={8} lg={8}>
+                                        <div className="nutrition-type-item">
+                                     
+                                                <img className="stock-icon" src={stock_image} alt="food stock" />
+                                           
+                                            <h4>Towels</h4>
+                                            <h5>Dogs can get nauseous and anxious during their first few trips in a car. Spread a few towels down to help catch / cleanup any mess</h5>
+                                        </div>
+                                    </Col>
+                                    <Col
+                                        className="gutter-row" xs={24} sm={24} md={8} lg={8}>
+                                        <div className="nutrition-type-item">
+                                     
+                                                <img className="stock-icon" src={stock_image} alt="food stock" />
+                                           
+                                            <h4>A friend</h4>
+                                            <h5>It will be a stressful drive home for you new pup. If possible, have a friend come with to reassure pup while you drive</h5>
+                                        </div>
+                                    </Col>
+                                    <Col
+                                        className="gutter-row" xs={24} sm={24} md={8} lg={8}>
+                                        <div className="nutrition-type-item">
+                                     
+                                                <img className="stock-icon" src={stock_image} alt="food stock" />
+                                           
+                                            <h4>Russell's gear</h4>
+                                            <h5>Bring Russell's leash, harness, collar, tags, and some poo bags</h5>
+                                        </div>
+                                    </Col>
+                                   
+                                </Row>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="next-section-button dark-yellow">
+                    <div>
+                            <h4>Ready for the big pickup?</h4>
+                            <h5>In the next section we talk about your first few hours with pup! Let's go!</h5>
+                        </div>
+                        <Link to='/guide/pup-home'>
+                            <Button>
+                                Step 3: Pup is home. Help!
+                                    </Button>
+                        </Link>
+
+                    </div>
+
+
+                   
                 </Layout>
             </div>
         )
--- src/Routes/GuidePages/PrepareHome.js
+++ src/Routes/GuidePages/PrepareHome.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import YouTube from 'react-youtube';
 import { Link } from 'react-router-dom'
-import { Layout, Content, Button } from 'antd';
+import { Layout, Content, Button, Row, Col } from 'antd';
 import { LeftOutlined, RightOutlined, BulbOutlined, ToolOutlined, ClearOutlined } from '@ant-design/icons';
+import food_image from '../../Assets/food_image.png'
+
 
 
 class PrepareHome extends React.Component {
+    componentDidMount = () => {
+        window.scrollTo(0, 0);
+      }
+    
     _onReady(event) {
         // access to player in all event handlers via event.target
         event.target.pauseVideo();
@@ -23,7 +29,7 @@ class PrepareHome extends React.Component {
         };
 
         return (
-            <Layout>
+            <Layout className="white">
                 <div className="survival-guide-banner">
 
                     <div className="survival-guide-banner-image white" />
@@ -48,76 +54,122 @@ class PrepareHome extends React.Component {
                 </div>
 
                 <div className="content-section dark-yellow">
-                        <div className="checklist-page content">
-                            <div className="checklist-page-alert font-black">
+                    <div className="checklist-page content">
+                        <div className="checklist-page-alert font-black">
+
+                            <h3>What you need to know</h3>
+                            <h5>It's time to puppy-proof your home. We'll explain how to make your home safe and inviting for pup, and to set up a safe place for him to sleep</h5>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="content-section white">
+                    <div className="checklist-page content space grain-free">
+                        <Row gutter={[40, 24]} className="grain">
+                            <Col className="gutter-row" xs={24} sm={24} md={12} lg={12}>
+                                <div className="grain-content">
+                                    <h4>How to puppy proof</h4>
+                                    <div className="content-group">
+                                        <h6>Hide any items that can be chewed or swallowed</h6>
+
+                                        <p>Socks, baby toys, toilet paper. Your pup will be an expert at getting into anything. Make sure everything is put away where pup can't access it</p>
+                                    </div>
+
+                                    <div className="content-group">
+                                        <h6>Put away anything that can be knocked over easily</h6>
+
+                                        <p>Think heavy things that would hurt pup if toppled over</p>
+                                    </div>
+
+                                    <div className="content-group">
+                                        <h6>Safely store medications, cleaning supplies, etc</h6>
+
+                                        <p>Nothing should be kept open where pup could get it</p>
+                                    </div>
+
+
+                                    
+                                </div>
+                            </Col>
+                            <Col className="gutter-row" xs={24} sm={24} md={12} lg={12}>
+                                <img className="food-image" src={food_image} alt="food stock" />
+                            </Col>
+                        </Row>
+
+                    </div>
+                </div>
+
+                <div className="content-section light-purple">
+                    <div className="checklist-page content space grain-free">
+                        <Row style={{ margin: 0 }}gutter={[40, 24]} className="grain">
+                            <Col className="gutter-row" xs={24} sm={24} md={24} lg={12}>
                                
-                                <h3>What you need to know</h3>
-                                <h5>It's time to puppy-proof your home. We'll explain how to make your home safe and inviting for pup, and to set up a safe place for him to sleep</h5>
-                            </div>
+                                <YouTube videoId="n6MHdWFnU7E" opts={opts} onReady={this._onReady} />
+                            </Col>
+                            <Col className="gutter-row" xs={24} sm={24} md={12} lg={12}>
+                            <div className="grain-content">
+                                <div className="row-content-intro">
+                                    <h4>Set up your crate</h4>
+                                    <h5>Watch this video for instructions on how to set up your crate</h5>
+                                </div>
+                                   
+                                    <h5>Wait! This crate looks like a jail cell!</h5>
+                                    <p>Force-free dog trainers agree that crates are important for puppies. While they make look like a jail, they actually provide a safe, enclosed space mimicking a den which they are used to</p>
+                                    <h5>Ever notice how dogs tend to find small, enclosed spaces to nap or sleep?</h5>
+                                    <p>Dogs need small spaces to feel safe. During a stressful time like moving to a forever home, a crate becomes a safe haven for your dog</p>
+                                 
 
-                        </div>
+
+                                    
+                                </div>
+                                
+                            </Col>
+                            
+                            
+                        </Row>
+
                     </div>
-                
-               
-                <Content
-                    //style={{ padding: '0 40px' }}
-                    className="page-content">
+                </div>
+                <div className="content-section light-purple">
+                    <div className="checklist-page content space grain-free">
+                        <Row style={{ margin: 0 }}gutter={[40, 24]} className="grain">
+                            
+                            <Col className="gutter-row" xs={24} sm={24} md={12} lg={12}>
+                            <div className="grain-content">
+                                <div className="row-content-intro">
+                                    <h4>Pro Tip</h4>
+                                    <h5>Drape a blanket over the crate, except for the opening, to provide an extra sense of security for your pup</h5>
+                                </div>
+                                   
+                    
 
 
+            
+                                </div>
+                                
+                            </Col>
+                            <Col className="gutter-row" xs={24} sm={24} md={24} lg={12}>
+                               
+                                <img className="stock-image" src="https://www.rover.com/blog/wp-content/uploads/2020/01/61d4qnb48GL.jpg" alt="crate cover" />
+                            </Col>
+                        </Row>
 
-
+                    </div>
+                </div>
+                <div className="next-section-button dark-yellow">
                     <div>
-                        <h3>Prepare your home</h3>
-                        <div>
-                            Task Overview
-                    <ul>
-                                <li>Clean up any items that can be chewed or swallowed</li>
-                                <li>Set up your crate</li>
-                                <li>Put out bowls and toys</li>
-
-                            </ul>
+                            <h4>Home feeling a bit safer for pup?</h4>
+                            <h5>Let's get you ready to pick up Russell!</h5>
                         </div>
-
-                        <div className="icon-box">
-                            <ClearOutlined className="prepare-home-icon" />
-                        </div>
-
-                        <div>
-                            <h4>Hide any items that can be chewed</h4>
-                            <p>Dogs will chew anything - including your furniture! Make sure to hide small itmems that can be swallowed and anything that you don't want your pup getting into</p>
-                        </div>
-
-                        <div className="icon-box">
-                            <ToolOutlined className="prepare-home-icon" />
-                        </div>
-                        <h4>Set up your crate</h4>
-                        <p>Choose the location of your crate. Will your puppy be sleeping in your bedroom or in another room?</p>
-                        <h5>Watch this video on how to set your crate</h5>
-
-                        <div>
-                            <YouTube videoId="n6MHdWFnU7E" opts={opts} onReady={this._onReady} />
-                        </div>
-
-                        <div>
-                            <div className="icon-box">
-                                <BulbOutlined className="prepare-home-icon" />
-                            </div>
-
-                            <h5>Place a blanket over the crate, leaving only the front uncovered </h5>
-                            <img className="page-image" src="https://www.rover.com/blog/wp-content/uploads/2020/01/61d4qnb48GL.jpg" alt="crate cover" />
-                        </div>
-
-                    </div>
-                    <div className="next-section-button">
-                        <Link to='/food'>
+                        <Link to='/guide/pickup-pup'>
                             <Button>
-                                Up Next: Pick up pup
+                                Step 2: Pick up pup
                                     </Button>
                         </Link>
 
-
                     </div>
-                </Content>
+                  
             </Layout>
         )
     }
