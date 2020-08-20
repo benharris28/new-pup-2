1cf7d4dc2d44b20723e994b548209e093a618a4c
Ben Harris <benjaminharris28@gmail.com>
2020-08-20 13:58:28 -0400

    updated

--- src/Routes/LandingPage/LandingPage.js
+++ src/Routes/LandingPage/LandingPage.js
@@ -35,24 +35,27 @@ class LandingPage extends React.Component {
 
         return (
             <div>
-                <div className="grid-halves-section">
-                    <div className="grid-section-half bg-primary-4">
-                        <div className="container">
-                            <div className="grid-text-content">
-                                <h1 className="display-heading-1">Bringing home a doggo?</h1>
-                                <p className="text-large">Test content test content test content</p>
-                                <Button className="menu-button signup">
-                                    Test
-                                </Button>
-                            </div>
+                <div className="hero">
+                    <div className="container">
+                        <div className="title-centre">
+                            <h1 className="display-heading">
+                                Bringing home a doggo?
+                            </h1>
+                            <div className="hero-details">
+                            From a step-by-step guide of what you need, to expert picks of food, to a customized surivival guide, we've got your back.
                         </div>
-                    </div>
-                    <div className="grid-section-half pg-primary-3">
-                        <img src={russ_headshot} alt="vet" className="top-layer" />
-                        <div class="interface-screenshot-decoration bg-primary-5 round-bottom-right"></div>
-                    </div>
+                        <div className="button-wrapper">
+                            <Button className="banner-button">
+                                Start Now
+                            </Button>
+                        </div>
 
+                        </div>
+                        
+                    </div>
+                    
                 </div>
+                
                 <div className="guide-banner">
 
                     <div className="guide-banner-image dark-yellow" />
@@ -73,7 +76,7 @@ class LandingPage extends React.Component {
                     </div>
                 </div>
 
-                <div className="section light-yellow">
+                <div className="section white">
                     <div className="main-container">
                         <Row style={{ margin: 0 }} gutter={[40, 24]} className="grain">
 
@@ -81,7 +84,7 @@ class LandingPage extends React.Component {
                                 <div className="container align-center">
                                     <div className="row-content-intro">
                                         <h2>Expert recommended products</h2>
-                                        <h5>We'll tell you only what you need and exactly where to find it</h5>
+                                        <p className="paragraph">We'll tell you only what you need and exactly where to find it</p>
                                         <Link to='/checklist'>
                                             <Button className="banner-button">
                                                 Start now
@@ -116,7 +119,7 @@ class LandingPage extends React.Component {
                                     <div className="row-content-intro">
                                         <h2>There are thousands of dog foods </h2>
                                         <h4>We'll narrow it down to 2</h4>
-                                        <h5>Our team of experts have done the research and made your choice really. really easy</h5>
+                                        <p className="paragraph">Our team of experts have done the research and made your choice really. really easy</p>
                                         <Link to='/checklist'>
                                             <Button className="banner-button">
                                                 Start now
@@ -151,7 +154,7 @@ class LandingPage extends React.Component {
                                 <div className="container align-center">
                                     <div className="row-content-intro">
                                         <h2>We'll show the you closest vets to you</h2>
-                                        <h5>All "vetted" by our team of experts</h5>
+                                        <p className="paragraph">All "vetted" by our team of experts</p>
 
                                         <Link to='/checklist'>
                                             <Button className="banner-button">
@@ -187,7 +190,7 @@ class LandingPage extends React.Component {
                                 <div className="container align-center">
                                     <div className="row-content-intro">
                                         <h2>We'll help you navigate the first 24 hours with doggo</h2>
-                                        <h5>House training, pup's first meal, training. We've got you covered.</h5>
+                                        <p className="paragraph">House training, pup's first meal, training. We've got you covered.</p>
 
                                         <Link to='/checklist'>
                                             <Button className="banner-button">
