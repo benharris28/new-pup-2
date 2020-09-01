45cc2a144a665f76ba1b654a967a21df5cfaa9fd
Ben Harris <benjaminharris28@gmail.com>
2020-08-31 22:04:17 -0400

    updated formatting and content of guide home page

--- src/Routes/SurvivalGuide/SurvivalGuide.js
+++ src/Routes/SurvivalGuide/SurvivalGuide.js
@@ -6,6 +6,12 @@ import { CaretRightOutlined, UserOutlined, HomeOutlined, CarOutlined } from '@an
 import food_image from '../../Assets/food_image.png'
 import survival_guide_headshot from '../../Assets/survival_guide_headshot.png'
 import dog_wellness_dash from '../../Assets/dog_wellness_dash.png'
+import dog_trick from '../../Assets/dog_trick.png'
+import dog_in_car from '../../Assets/dog_in_car.jpg'
+import clean_up from '../../Assets/clean_up.jpg'
+import dog_eating from '../../Assets/dog_eating.jpg'
+import dog_pee from '../../Assets/dog_pee.png'
+
 
 
 
@@ -33,29 +39,40 @@ class SurvivalGuide extends React.Component {
 
     const points = [
       {
-        image: <img className="food-image" src={food_image} alt="stock" />,
+        image: <img className="guide-item-image" src={clean_up} alt="dog running" />,
         step: 'Step 1',
-        title: 'Get your home ready for Russell'
+        title: 'Get your home ready for Russell',
+        description: 'Time to hide all those small objects!',
+        link: '/guide/prepare-home'
       },
       {
-        image: <img className="food-image" src={food_image} alt="stock" />,
+        image: <img className="guide-item-image" src={dog_in_car} alt="stock" />,
         step: 'Step 2',
-        title: 'Pick up Russell'
+        title: 'Pick up Russell',
+        description: 'The adventure begins!',
+        link: '/guide/pickup-pup'
+
       },
       {
-        image: <img className="food-image" src={food_image} alt="stock" />,
+        image: <img className="guide-item-image" src={dog_trick} alt="man giving high five to dog" />,
         step: 'Step 3',
-        title: 'Teach Russell a few tricks'
+        title: 'Teach Russell a few tricks',
+        description: 'It is easier thank you think!',
+        link: '/guide/pup-home'
       },
       {
-        image: <img className="food-image" src={food_image} alt="stock" />,
+        image: <img className="guide-item-image" src={dog_eating} alt="dog next to food bowl" />,
         step: 'Step 4',
-        title: 'Give Russell his first meal'
+        title: 'Give Russell his first meal',
+        description: 'We will lay out an easy feeding schedule and answer all your questions',
+        link: '/guide/meal-time'
       },
       {
-        image: <img className="food-image" src={food_image} alt="stock" />,
+        image: <img className="guide-item-image" src={dog_pee} alt="dog peeing on fire hydrant" />,
         step: 'Step 5',
-        title: 'Housetrain Russell'
+        title: 'Housetrain Russell',
+        description: 'We will walk you through the easiest way to teach your doggo to pee outside',
+        link: '/guide/crate-train'
       },
 
 
@@ -148,73 +165,40 @@ class SurvivalGuide extends React.Component {
                     <div className="content-section white">
                     <div className="content-container">
                             <div className="title-wrapper">
-                                <h2>Russell's Doggo Shop</h2>
+                                <h2>Survival Guide Categories</h2>
                             </div>
                             <div className="category-section">
-                            <div className="title-wrapper">
-                                <h2>Subscriptions</h2>
-                                <h5>Russell is subscribed to the following products</h5>
-                            </div>
+                         
                             <div className="product-loop">
-                                
-                                    <div className="shop-product">
+
+                                 {points.map(point =>
+                                    <div className="guide-item">
                                         <div role="listitem" className="shop-product-item-container">
                                             <Link to='/checklist'>
-                                                <div className="product-item">
-                                                    <div className="shop-product-image-box">
-                                                        <img className="shop-product-image" src={dog_wellness_dash} alt="dog running" />
+                                                <div className="guide-list-item">
+                                                    <div className="guide-item-image-box">
+                                                        {point.image}
                                                     </div>
-                                                    <div className="shop-product-content">
+                                                    <div className="guide-overview-content">
                                                       
-                                                        <h4 className="feature-heading">Product 1</h4>
-                                                        <p className="newsfeed-item-detail">Details about this product</p>
+                                                        <h4 className="feature-heading">{point.title}</h4>
+                                                        <p className="newsfeed-item-detail">{point.description}</p>
                                                     </div>
-                                                    <div className="shop-product-button-box">
-                                                        <Button className="button-cta">
-                                                            Product Details
+                                                    <div className="guide-item-button-box">
+                                                        <Button className="guide-item-button">
+                                                            Start
                                                         </Button>
                                                     </div>
                                                 </div>
                                             </Link>
                                         </div>
                                     </div>
+                                 )}
                            
                             
                                  
-                                    <div className="shop-product">
-                                        <div role="listitem" className="shop-product-item-container">
-                                            <Link to='/checklist'>
-                                                <div className="product-item">
-                                                    <div className="shop-product-image-box">
-                                                        <img className="shop-product-image" src={dog_wellness_dash} alt="dog running" />
-                                                    </div>
-                                                    <div className="shop-product-content">
-                                                      
-                                                        <h4 className="feature-heading">Product 1</h4>
-                                                        <p className="newsfeed-item-detail">Details about this product</p>
-                                                    </div>
-                                                </div>
-                                            </Link>
-                                        </div>
-                                    </div>
+                                  
                              
-                                    <div className="shop-product">
-                                        <div role="listitem" className="shop-product-item-container">
-                                            <Link to='/checklist'>
-                                                <div className="product-item">
-                                                    <div className="shop-product-image-box">
-                                                        <img className="shop-product-image" src={dog_wellness_dash} alt="dog running" />
-                                                    </div>
-                                                    <div className="shop-product-content">
-                                                      
-                                                        <h4 className="feature-heading">Product 1</h4>
-                                                        <p className="newsfeed-item-detail">Details about this product</p>
-                                                    </div>
-                                                </div>
-                                            </Link>
-                                        </div>
-                                    </div>
-                            
 
                             </div>
                             </div>
@@ -223,45 +207,6 @@ class SurvivalGuide extends React.Component {
 
    
           
-                    <div className="checklist-page content">
-                            <div className="checklist-page-section-intro">
-                                <h3>Here are the steps</h3>
-                                <h5>We broke it down into five easy steps in the order that you'll need to do them to be ready</h5>
-                            </div>
-                            <div className="nutrition-types-container">
-                            
-                                <Row
-                                    gutter={[40, 24]}
-                                >
-                                  {points.map(point =>
-
-                                    <Col
-                                        className="gutter-row" xs={24} sm={24} md={12} lg={12}>
-                                        <div className="nutrition-type-item">
-                                                
-                                                {point.image}
-                                            <h5 className="survival-guide-step">{point.step}</h5>
-                                            <h4>{point.title}</h4>
-                                            
-                                        </div>
-                                    </Col>
-                                    )}
-                                  </Row>
-                            </div>
-                        </div>
-                    
-                        <div className="next-section-button coral">
-                    <div>
-                            <h4>Are you ready?</h4>
-                            <h5>Let's jump right in and get your home ready for pup!</h5>
-                        </div>
-                        <Link to='/guide/prepare-home'>
-                            <Button>
-                                Step 1: Prepare your home
-                                    </Button>
-                        </Link>
-
-                    </div>
 
           
         </Layout>
