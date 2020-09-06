d31e783397689c2372b92f3471b89408f0b9d5fa
Ben Harris <benjaminharris28@gmail.com>
2020-09-06 15:16:23 -0400

    major updates to landing page

--- src/Routes/LandingPage/LandingPage.js
+++ src/Routes/LandingPage/LandingPage.js
@@ -13,6 +13,10 @@ import vet_checklist from '../../Assets/vet_checklist.png'
 import dog_food_checklist from '../../Assets/dog_food_checklist.png'
 import crate_landing_page from '../../Assets/crate_landing_page.png'
 import guide_checklist from '../../Assets/guide_checklist.png'
+import russ_puppy_no_background from '../../Assets/russ_puppy_no_background.png'
+import dog_empty_bowl from '../../Assets/dog_empty_bowl.jpg'
+import { WarningOutlined, InfoCircleOutlined, ExclamationCircleOutlined, CheckOutlined } from '@ant-design/icons';
+
 
 class LandingPage extends React.Component {
     state = {
@@ -36,29 +40,155 @@ class LandingPage extends React.Component {
 
         return (
             <div>
-                <div className="hero light-blue">
-                    <div className="container">
-                        
-                        <div className="title-centre">
+                 <div className="content-section light-grey">
+                    <div className="content-container">
+
+                    <div className="title-centre">
+                    
+                         
                             <h1 className="display-heading">
-                                Bringing home a doggo?
+                                Doggo
                             </h1>
                             <div className="hero-details">
-                            From a step-by-step guide of what you need, to expert picks of food, to a customized surivival guide, we've got your back.
+                            Everything you need for your best friend - in one place
                         </div>
-                        <div className="button-wrapper">
-                            <Button className="banner-button">
-                                Start Now
-                            </Button>
-                        </div>
+                          
+                       
 
                         </div>
-                        
+                      
+                        </div>
+                        </div>
+
+                        <div className="content-section low-padding light-coral">
+                    <div className="content-container">
+                     
+                        <div className="topics-container">
+                            <div className="topic-item">
+                                Find a vet
+                            </div>
+                            <div className="topic-item">
+                                Obedience trainers
+                            </div>
+                            <div className="topic-item"> 
+                                Find the best food
+                            </div>
+                            <div className="topic-item"> 
+                                Find the best gear
+                            </div>
+                        </div>
                     </div>
-                    
+                    </div>
+
+                    <div className="content-section white">
+                        <div className="content-container">
+                            <div className="title-centre">
+                                <div className="guide-heading">
+                                    Gear Shop
+                            </div>
+                                <h1 className="display-heading">
+                                    Outfit your doggo in one-click 
+                            </h1>
+                             
+
+
+
+
+
+                            </div>
+                       
+
+                        </div>
+                    </div>
+
+                    <div className="no-padding-section">
+                    <div className="content-container">
+                
+                        <div className="grid-container">
+                        <Row style={{ margin: 0 }} gutter={[40, 24]} justify="space-evenly">
+                        <Col className="gutter-row" xs={{ span: 24, order: 2 }} sm={{ span: 24, order: 2 }} md={{ span: 12, order: 1 }} lg={{ span: 12, order: 1 }} style={{ padding: 0 }}>
+                                    <div className="grid-content">
+                                 
+                                        
+                                    <div className="title-left">
+                                    <h6 className="display-heading-intro font-coral">New Puppy</h6>
+                            <h3 className="display-sub-heading">
+                                New Puppy Guide
+                            </h3>
+                           
+                            <div className="hero-details">
+                            Bringing home a new pup? Start our step-by-step guide to outfit your dog, find them great food, the best vet and puppy classes in your area, and an interactive survival guide!
+                          
+                       
+
+                        </div>
+                        <Link to='/checklist'>
+                                            <Button className="banner-button">
+                                                Start now
+            </Button>
+                                        </Link>
+                       
+
+                        </div>
+                                    
+                                       
+                                    </div>
+                                </Col>
+                                <Col className="gutter-row" xs={{ span: 24, order: 1 }} sm={{ span: 24, order: 1 }} md={{ span: 12, order: 2 }} lg={{ span: 12, order: 2 }} style={{ padding: 0, textAlign: "center" }}>
+                                    <div className="grid-image-container" style={{ padding: 0}}>
+                                    <img style={{ width: 300 }} className="grid-image" src={russ_puppy_no_background} alt="puppy sitting on command" />
+                                    </div>
+                                    
+                                </Col>
+                            </Row>
+                        </div>
+                        </div>
                 </div>
+
+
                 
                
+                <div className="content-section low-padding white">
+                    <div className="content-container">
+                    
+                        <div className="grid-container">
+                        <Row style={{ margin: 0 }} gutter={[40, 24]} justify="space-evenly">
+                        <Col className="gutter-row" xs={{ span: 24, order: 2 }} sm={{ span: 24, order: 2 }} md={{ span: 10, order: 1 }} lg={{ span: 8, order: 1 }} style={{ padding: 0 }}>
+                                    <div className="grid-content">
+                                 
+                                        
+                                    <div className="title-left">
+                    
+                            <h6 className="display-heading-intro font-coral">Nutrition</h6>
+                            <h1 className="display-sub-heading">
+                                Food Advisor
+                            </h1>
+                           
+                            <div className="hero-details">
+                            Use our dog food advisor tool to find the healthiest food for your doggo
+                        </div>
+                        <Link to='/checklist'>
+                                            <Button className="banner-button">
+                                                Start now
+            </Button>
+                                        </Link>
+                       
+
+                        </div>
+                                    
+                                       
+                                    </div>
+                                </Col>
+                                <Col className="gutter-row" xs={{ span: 24, order: 1 }} sm={{ span: 24, order: 1 }} md={{ span: 14, order: 2 }} lg={{ span: 16, order: 2 }} style={{ padding: 0 }}>
+                                    <div className="grid-image-container">
+                                    <img className="grid-image" src={dog_empty_bowl} alt="puppy sitting on command" />
+                                    </div>
+                                    
+                                </Col>
+                            </Row>
+                        </div>
+                        </div>
+                </div>
 
                 <div className="section white">
                     <div className="main-container">
