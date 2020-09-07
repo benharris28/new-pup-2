1706bd9b0e11986914e305b60a944ea8d36b3f9c
Ben Harris <benjaminharris28@gmail.com>
2020-09-06 21:44:08 -0400

    updated

--- src/Routes/LandingPage/LandingPage.js
+++ src/Routes/LandingPage/LandingPage.js
@@ -15,6 +15,7 @@ import crate_landing_page from '../../Assets/crate_landing_page.png'
 import guide_checklist from '../../Assets/guide_checklist.png'
 import russ_puppy_no_background from '../../Assets/russ_puppy_no_background.png'
 import dog_empty_bowl from '../../Assets/dog_empty_bowl.jpg'
+import dog_groom from '../../Assets/dog_groom.jpg'
 import { WarningOutlined, InfoCircleOutlined, ExclamationCircleOutlined, CheckOutlined } from '@ant-design/icons';
 
 
@@ -39,7 +40,7 @@ class LandingPage extends React.Component {
         const width = window.addEventListener('resize', this.handleResize);
 
         return (
-            <div>
+            <div className="white">
                  <div className="content-section light-grey">
                     <div className="content-container">
 
@@ -65,16 +66,16 @@ class LandingPage extends React.Component {
                      
                         <div className="topics-container">
                             <div className="topic-item">
-                                Find a vet
+                                Gear Shop
                             </div>
                             <div className="topic-item">
-                                Obedience trainers
+                                Food Shop
                             </div>
                             <div className="topic-item"> 
-                                Find the best food
+                                Vet Finder
                             </div>
                             <div className="topic-item"> 
-                                Find the best gear
+                                Learn
                             </div>
                         </div>
                     </div>
@@ -101,7 +102,7 @@ class LandingPage extends React.Component {
                         </div>
                     </div>
 
-                    <div className="no-padding-section">
+                    <div className="no-padding-section light-grey">
                     <div className="content-container">
                 
                         <div className="grid-container">
@@ -151,7 +152,7 @@ class LandingPage extends React.Component {
                 <div className="content-section low-padding white">
                     <div className="content-container">
                     
-                        <div className="grid-container">
+                        <div className="grid-container" style={{ marginTop: 0}}>
                         <Row style={{ margin: 0 }} gutter={[40, 24]} justify="space-evenly">
                         <Col className="gutter-row" xs={{ span: 24, order: 2 }} sm={{ span: 24, order: 2 }} md={{ span: 10, order: 1 }} lg={{ span: 8, order: 1 }} style={{ padding: 0 }}>
                                     <div className="grid-content">
@@ -181,7 +182,7 @@ class LandingPage extends React.Component {
                                 </Col>
                                 <Col className="gutter-row" xs={{ span: 24, order: 1 }} sm={{ span: 24, order: 1 }} md={{ span: 14, order: 2 }} lg={{ span: 16, order: 2 }} style={{ padding: 0 }}>
                                     <div className="grid-image-container">
-                                    <img className="grid-image" src={dog_empty_bowl} alt="puppy sitting on command" />
+                                    <img className="grid-image-langing-page" src={dog_empty_bowl} alt="puppy sitting on command" />
                                     </div>
                                     
                                 </Col>
@@ -190,150 +191,56 @@ class LandingPage extends React.Component {
                         </div>
                 </div>
 
-                <div className="section white">
-                    <div className="main-container">
-                        <Row style={{ margin: 0 }} gutter={[40, 24]} className="grain">
-
-                            <Col className="gutter-row" xs={{ span: 24, order: 2 }} sm={{ span: 24, order: 2 }} md={{ span: 12, order: 1 }} lg={{ span: 12, order: 1 }} style={{ paddingLeft: 0 }}>
-                                <div className="container align-center">
-                                    <div className="row-content-intro">
-                                        <h2>Expert recommended products</h2>
-                                        <p className="paragraph">We'll tell you only what you need and exactly where to find it</p>
-                                        <Link to='/checklist'>
-                                            <Button className="banner-button">
-                                                Start now
-            </Button>
-                                        </Link>
-                                     
-                                    </div>
-
-
-
-
-
-                                </div>
-                                
-                            </Col>
-                            <Col className="gutter-row" xs={{ span: 24, order: 1 }} sm={{ span: 24, order: 1 }} md={{ span: 12, order: 12 }} lg={{ span: 12, order: 2 }} style={{ padding: 0 }}>
-                                <div className="portrait-image-container align-center">
-                                    <img className="grid-image" src={crate_landing_page} alt="dog crate" />
-                                    <div className="bg-element bg-yellow">
+                <div className="content-section low-padding white">
+                    <div className="content-container">
+                    
+                        <div className="grid-container" style={{ marginTop: 0}}>
+                        <Row style={{ margin: 0 }} gutter={[40, 24]} justify="space-evenly">
+                        <Col className="gutter-row" xs={{ span: 24, order: 2 }} sm={{ span: 24, order: 2 }} md={{ span: 10, order: 2 }} lg={{ span: 8, order: 2 }} style={{ padding: 0 }}>
+                                    <div className="grid-content">
+                                 
                                         
-                                        </div>
-                                </div>
-
-                            </Col>
-                        </Row>
-
-                    </div>
-                </div>
-
-                <div className="section white">
-                    <div className="main-container">
-                        <Row style={{ margin: 0 }} gutter={[40, 24]} className="grain">
-
-                            <Col className="gutter-row" xs={{ span: 24, order: 2 }} sm={{ span: 24, order: 2 }} md={{ span: 12, order: 1 }} lg={{ span: 12, order: 1 }} style={{ paddingLeft: 0 }}>
-                                <div className="container align-center">
-                                    <div className="row-content-intro">
-                                        <h2>There are thousands of dog foods </h2>
-                                        <h4>We'll narrow it down to 2</h4>
-                                        <p className="paragraph">Our team of experts have done the research and made your choice really. really easy</p>
-                                        <Link to='/checklist'>
+                                    <div className="title-left">
+                    
+                            <h6 className="display-heading-intro font-coral">Professionals</h6>
+                            <h1 className="display-sub-heading">
+                                Find trainers, groomers, and vets in your area
+                            </h1>
+                           
+                            <div className="hero-details">
+                            We did the work to find the highest rated professionals in your area
+                        </div>
+                        <Link to='/checklist'>
                                             <Button className="banner-button">
                                                 Start now
             </Button>
                                         </Link>
+                       
+
+                        </div>
+                                    
+                                       
                                     </div>
-
-
-
-
-
-                                </div>
-
-                            </Col>
-                            <Col className="gutter-row" xs={{ span: 24, order: 1 }} sm={{ span: 24, order: 1 }} md={{ span: 12, order: 12 }} lg={{ span: 12, order: 2 }} style={{ padding: 0 }}>
-                                <div className="portrait-image-container align-center">
-                                    <img className="grid-image" src={dog_food_checklist} alt="dog food" />
-                                </div>
-
-                            </Col>
-                        </Row>
-
-                    </div>
+                                </Col>
+                                <Col className="gutter-row" xs={{ span: 24, order: 1 }} sm={{ span: 24, order: 1 }} md={{ span: 14, order: 1 }} lg={{ span: 16, order: 1 }} style={{ padding: 0 }}>
+                                    <div className="grid-image-container">
+                                    <img className="grid-image" src={dog_groom} alt="puppy sitting on command" />
+                                    </div>
+                                    
+                                </Col>
+                            </Row>
+                        </div>
+                        </div>
                 </div>
 
-
-                <div className="section white">
-                    <div className="main-container">
-                        <Row style={{ margin: 0 }} gutter={[40, 24]} className="grain">
-
-                            <Col className="gutter-row" xs={{ span: 24, order: 2 }} sm={{ span: 24, order: 2 }} md={{ span: 12, order: 1 }} lg={{ span: 12, order: 1 }} style={{ paddingLeft: 0 }}>
-                                <div className="container align-center">
-                                    <div className="row-content-intro">
-                                        <h2>We'll show the you closest vets to you</h2>
-                                        <p className="paragraph">All "vetted" by our team of experts</p>
-
-                                        <Link to='/checklist'>
-                                            <Button className="banner-button">
-                                                Start now
-            </Button>
-                                        </Link>
-                                    </div>
+                
+        
 
 
+               
 
 
-
-                                </div>
-
-                            </Col>
-                            <Col className="gutter-row" xs={{ span: 24, order: 1 }} sm={{ span: 24, order: 1 }} md={{ span: 12, order: 12 }} lg={{ span: 12, order: 2 }} style={{ padding: 0 }}>
-                                <div className="portrait-image-container align-center">
-                                    <img className="grid-image" src={vet_checklist} alt="vet tools" />
-                                </div>
-
-                            </Col>
-                        </Row>
-
-                    </div>
-                </div>
-
-
-                <div className="section white">
-                    <div className="main-container">
-                        <Row style={{ margin: 0 }} gutter={[40, 24]} className="grain">
-
-                            <Col className="gutter-row" xs={{ span: 24, order: 2 }} sm={{ span: 24, order: 2 }} md={{ span: 12, order: 1 }} lg={{ span: 12, order: 1 }} style={{ paddingLeft: 0 }}>
-                                <div className="container align-center">
-                                    <div className="row-content-intro">
-                                        <h2>We'll help you navigate the first 24 hours with doggo</h2>
-                                        <p className="paragraph">House training, pup's first meal, training. We've got you covered.</p>
-
-                                        <Link to='/checklist'>
-                                            <Button className="banner-button">
-                                                Start now
-            </Button>
-                                        </Link>
-                                    </div>
-
-
-
-
-
-                                </div>
-
-                            </Col>
-                            <Col className="gutter-row" xs={{ span: 24, order: 1 }} sm={{ span: 24, order: 1 }} md={{ span: 12, order: 12 }} lg={{ span: 12, order: 2 }} style={{ padding: 0 }}>
-                                <div className="portrait-image-container align-center">
-                                    <img className="stock-image" src={guide_checklist} alt="compass" />
-                                </div>
-
-                            </Col>
-                        </Row>
-
-                    </div>
-                </div>
+                
 
                 <div className="cta-section dark-purple">
                     <div className="cta">
